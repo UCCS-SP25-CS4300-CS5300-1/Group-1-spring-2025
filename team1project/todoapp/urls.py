37580c96177e0f43742fd11bf68562d908fdc013
James <jhpipboy@gmail.com>
2025-03-12 00:49:36 +0000

    Login and Logout work, but html needs to be re-worked

--- team1project/todoapp/urls.py
+++ team1project/todoapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import index
+from .views import index, ProfileSettings
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
 	path('', index, name='index'),
+	path('profile_settings/', ProfileSettings.as_view(), name='profile_settings'),
+	path('logout/', LogoutView.as_view(), name='logout')
 ]
--- team1project/todoapp/views.py
+++ team1project/todoapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views import View
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -13,3 +15,15 @@ def index(request):
 			return redirect("index")
 
 	return render(request, 'index.html', {'form': form})
+
+class ProfileSettings(View):
+	def get(self, request):
+		return render(request, "profile_settings.html")
+
+
+	def post(self, request):
+		print("Logging out")
+		if "logout" in request.POST:
+				logout(request)
+				messages.success(request, "You have been logged out.")
+				return redirect("index")
