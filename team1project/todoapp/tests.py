1c50ccb38b34c433eded674e0fd538e00e11c29d
Sam Allen <sallen21@uccs.edu>
2025-03-12 07:12:51 +0000

    Debugging CI Pipeline

--- team1project/todoapp/tests.py
+++ team1project/todoapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# simple test to use for debugging CI pipeline
+class SimpleTest(TestCase):
+    def test_simple_test(self):
+        self.assertEqual(1, 1)
