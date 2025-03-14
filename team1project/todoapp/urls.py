ddba403e9f42dce4284f1da19be231d45006767c
Noah Kabel <nkabel@uccs.edu>
2025-03-14 08:49:04 +0000

    Created task view page

--- team1project/todoapp/urls.py
+++ team1project/todoapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from .views import index, ProfileSettings, register
 from django.contrib.auth.views import LogoutView
+from . import views
+
 
 urlpatterns = [
 	path('', index, name='index'),
 	path('profile_settings/', ProfileSettings.as_view(), name='profile_settings'),
 	path('logout/', LogoutView.as_view(), name='logout'),
 	path('register/', register, name='register'),
+	path('tasks/', views.task_view, name='task_view'),
 ]
