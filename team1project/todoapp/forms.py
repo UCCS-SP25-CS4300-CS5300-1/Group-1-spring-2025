56f547aa4424c1a99f1dbdd82ca6977c30aab73d
James <jhpipboy@gmail.com>
2025-04-02 17:45:48 +0000

    Finish views test for accepting and declining a task

--- team1project/todoapp/forms.py
+++ team1project/todoapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from .models import Task, Category, TaskCollabRequest
 from django_select2.forms import ModelSelect2Widget
@@ -49,6 +50,7 @@ class TaskCollabForm(forms.ModelForm):
         # Exclude users who already have the task, shared, creator, or self
         user_queryset = User.objects.exclude(id__in=exclude_users)
 
+        # Prevent a request from being sent to people with existing request
         if self.task:
             existing_requests = TaskCollabRequest.objects.filter(
                 task=self.task,
@@ -56,33 +58,6 @@ class TaskCollabForm(forms.ModelForm):
             user_queryset = user_queryset.exclude(id__in=existing_requests)
 
         self.fields['to_user'].queryset = user_queryset
-
-    # Prevent the user from sending another request for the same task to the next user
-    def clean(self):
-        cleaned_data = super().clean()
-        to_user = cleaned_data.get('to_user')
-
-        if to_user and self.task and self.user:
-            # Prevent user from sending the request to the creator
-            if self.task.creator == to_user:
-                raise forms.ValidationError("You cannot send a request to the creator")
-
-            # Prevent user from sending a request to themselves
-            if self.user == to_user:
-                raise forms.ValidationError("You cannot send a request to yourself")
-            
-            # Prevent any user from sending requests to a user that already has a request
-            if TaskCollabRequest.objects.filter(
-                task=self.task,
-                to_user=to_user
-            ).exists():
-                raise forms.ValidationError("You have already sent a collaboration request to this user")
-            
-            # Prevent current user from sending a task request to a user who already has access to the task
-            if to_user.id in list(self.task.assigned_users):
-                raise forms.ValidationError("This task is already shared with the user")
-        
-        return cleaned_data
 
     class Meta:
         model = TaskCollabRequest
