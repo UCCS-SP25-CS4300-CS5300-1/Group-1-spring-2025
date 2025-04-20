a4fda7f06157be1d87385ea16eb9911d77f26352
Maryna Alsip <142547171+aquamariny@users.noreply.github.com>
2025-04-20 17:13:20 -0600

    Updated test_commands.py

--- team1project/todoapp/tests/test_commands.py
+++ team1project/todoapp/tests/test_commands.py
@@ -29,8 +29,9 @@ class SendTaskRemindersCommandTest(TestCase):
         self.assertEqual(mock_send_mail.call_count, 2)
         for call in mock_send_mail.call_args_list:
             args, kwargs = call
-            task_message_user1 = f"Hi {self.user1.username},\n\nYour task \"{self.task_due_soon.name}\" is due on {self.task_due_soon.due_date.strftime('%Y-%m-%d %H:%M')}.\n\nDon't forget to complete it."
-            task_message_user2 = f"Hi {self.user2.username},\n\nYour task \"{self.task_due_soon.name}\" is due on {self.task_due_soon.due_date.strftime('%Y-%m-%d %H:%M')}.\n\nDon't forget to complete it."
+            due_str = localtime(self.task_due_soon.due_date).strftime('%Y-%m-%d %H:%M')
+            task_message_user1 = f"Hi {self.user1.username},\n\nYour task \"{self.task_due_soon.name}\" is due on {due_str}.\n\nDon't forget to complete it."
+            task_message_user2 = f"Hi {self.user2.username},\n\nYour task \"{self.task_due_soon.name}\" is due on {due_str}.\n\nDon't forget to complete it."
             # Verify if the correct message is in the kwargs['message']
             self.assertTrue(task_message_user1 in kwargs['message'] or task_message_user2 in kwargs['message'])
             self.assertEqual(kwargs['from_email'], 'team1todo@gmail.com')
