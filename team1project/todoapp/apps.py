e446d588326bf67d8a1f557df97151e736376cb1
Tarek Fouad <tahmedfo@uccs.edu>
2025-05-04 03:48:55 +0000

    Linting in models.py

--- team1project/todoapp/apps.py
+++ team1project/todoapp/apps.py
@@ -5,5 +5,6 @@ Configure todoapp
 from django.apps import AppConfig
 
 class TodoappConfig(AppConfig):
+    ''' This is just the app config class '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todoapp'
